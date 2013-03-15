e1fb31917e7639b32913d0f9a13859ac380a5c65
Tomi <Tomi@me.com>
2013-03-15 20:51:20 +0100

    Added production server gunicorn.

--- findtofun/settings.py
+++ findtofun/settings.py
@@ -4,7 +4,7 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+#     ('Tomislav Slijepcevic', 'tomi.slij@gmail.com'),
 )
 
 MANAGERS = ADMINS
@@ -121,7 +121,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
