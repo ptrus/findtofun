5243f92d84a2eb22722b1a20dc1cb6f724d5b49e
Tomi <Tomi@me.com>
2013-03-26 23:30:21 +0100

    Dodan prefix za avtentikacija s Facebook-om.

--- findtofun/urls.py
+++ findtofun/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns('', 
     url(r'^app/', include('app.urls', namespace='app')),
-    url(r'^admin/', include(admin.site.urls))
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('fb.urls')),
 )
