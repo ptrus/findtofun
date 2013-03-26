53aca065817953a63dd1de7dcf49bf12810e0124
Peter <peter.us.99@gmail.com>
2013-03-27 00:02:50 +0100

    Removed unused imports

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,6 @@
 from django.conf.urls import patterns, url
-from django.views.generic import DetailView, ListView, TemplateView
-from django.utils import timezone
-from app import views
+from django.views.generic import DetailView, ListView
 from app.models import Event
-
-from app.models import Host
 
 urlpatterns = patterns('',
     url(r'^$',
@@ -18,5 +14,4 @@ urlpatterns = patterns('',
             model=Event,
             template_name='events/details.html'),
         name='detail'),
-
     )
--- app/views.py
+++ app/views.py
@@ -1,3 +1,1 @@
-from django.http import HttpResponse
-from django.template import Context, loader
 
