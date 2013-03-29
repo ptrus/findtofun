9ce258fba3e2aee4a4887c73aa4ce60d262a5696
Peter <peter.us.99@gmail.com>
2013-03-29 17:51:22 +0100

    needed view for logout

--- app/views.py
+++ app/views.py
@@ -1,1 +1,8 @@
+from django.contrib.auth import logout as auth_logout
+from django.http import HttpResponseRedirect
 
+
+def logout(request):
+    """Logs out user"""
+    auth_logout(request)
+    return HttpResponseRedirect('/')
