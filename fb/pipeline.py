c8d76de56013f2d348c510b0b436e3dc88f4d18c
Tomi <Tomi@me.com>
2013-04-01 23:31:03 +0200

    Odkomentiral sem query in omejil ?t. pridobljenih dogodkov.

--- fb/pipeline.py
+++ fb/pipeline.py
@@ -15,25 +15,25 @@ def write_extra_details(request, *args, **kwargs):
            SELECT eid FROM event_member 
            WHERE (uid IN (SELECT uid2 FROM friend WHERE uid1 = me())  OR uid = me())
            )
-        AND start_time > now()
+        AND start_time > now() LIMIT 20
         """
         
-#    query_results = graph.fql(query)
-#    if query_results.has_key("data"):
-#        events_data = query_results["data"]
-#        events_objects = []
-#        
-#        for event in events_data:
-#            host = event["host"]
-#            name = event["name"]
-#            location = event["location"]
-#            start_time = event["start_time"]
-#            end_time = event["end_time"]
-#            update_time = event["update_time"]
-#            
-#            events_objects.append(Event.objects.create_event(
-#                 None, name, location, start_time, end_time
-#             ))
-#            
-#        Event.objects.bulk_create(events_objects)    
+    query_results = graph.fql(query)
+    if query_results.has_key("data"):
+        events_data = query_results["data"]
+        events_objects = []
+        
+        for event in events_data:
+            host = event["host"]
+            name = event["name"]
+            location = event["location"]
+            start_time = event["start_time"]
+            end_time = event["end_time"]
+            update_time = event["update_time"]
+            
+            events_objects.append(Event.objects.create_event(
+                 host, name, location, start_time, end_time
+             ))
+            
+        Event.objects.bulk_create(events_objects)    
             
