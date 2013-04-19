84987951785a56dd882b2304a3e9b78359c20608
Tomi <Tomi@me.com>
2013-04-19 02:14:40 +0200

    Stvari sem prestavil v findtofun modul.

--- events/views.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth import logout as auth_logout
-from django.http import HttpResponseRedirect
-
-
-def logout(request):
-    """Logs out user"""
-    auth_logout(request)
-    return HttpResponseRedirect('/')
