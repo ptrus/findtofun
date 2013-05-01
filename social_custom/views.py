ae8a44fcf518553b7af3a8ed0914f13542395269
Tomi <Tomi@me.com>
2013-05-01 22:32:36 +0200

    Zbrisal viewe, ki jih več ne rabimo.

--- social_custom/views.py
+++ social_custom/views.py
@@ -1,39 +1,5 @@
 from django.http import HttpResponseRedirect
 from django.contrib.auth import logout as auth_logout
-from django.contrib.auth.decorators import login_required
-from django.template import RequestContext
-from django.shortcuts import render_to_response
-from django.contrib.messages.api import get_messages
-
-
-def home(request):
-    """Home view, displays login mechanism"""
-    if request.user.is_authenticated():
-        return HttpResponseRedirect('done')
-    else:
-        return render_to_response(
-            'index.html',
-            RequestContext(request))
-
-
-@login_required
-def done(request):
-    """Login complete view, displays user data"""
-    ctx = {
-        'last_login': request.session.get('social_auth_last_login_backend')
-    }
-    return render_to_response(
-        'done.html',
-        ctx, RequestContext(request))
-
-
-def error(request):
-    """Error view"""
-    messages = get_messages(request)
-    return render_to_response(
-        'error.html',
-        {'messages': messages},
-        RequestContext(request))
 
 
 def logout(request):
