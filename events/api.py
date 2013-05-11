0ee7d89dd360233f2d0effa59f885837acfaed3b
Peter <peter.us.99@gmail.com>
2013-05-11 12:39:22 +0200

    pep8 mixed spaces and tabs fixed

--- events/api.py
+++ events/api.py
@@ -6,50 +6,50 @@ from events.models import FbEvent, FbLocation, FbUser, FbEventFbUser
 
 
 class FbLocationResource(ModelResource):
-	class Meta:
-		queryset = FbLocation.objects.all()
-		resource_name = 'fblocation'
-		authentication = SessionAuthentication()
+    class Meta:
+        queryset = FbLocation.objects.all()
+        resource_name = 'fblocation'
+        authentication = SessionAuthentication()
 
 
 class FbUserResource(ModelResource):
-	class Meta:
-		queryset = FbUser.objects.all()
-		resource_name = 'fbuser'
-		authentication = SessionAuthentication()
+    class Meta:
+        queryset = FbUser.objects.all()
+        resource_name = 'fbuser'
+        authentication = SessionAuthentication()
 
 
 class FbEventFbUserResource(ModelResource):
-		fbuser = fields.ToOneField(FbUserResource, 'fbuser', full=True)
+    fbuser = fields.ToOneField(FbUserResource, 'fbuser', full=True)
 
-		class Meta:
-				queryset = FbEventFbUser.objects.all()
-				authentication = SessionAuthentication()
+    class Meta:
+        queryset = FbEventFbUser.objects.all()
+        authentication = SessionAuthentication()
 
 
 class FbEventResource(ModelResource):
-		venue = fields.ForeignKey(FbLocationResource, 'venue', full=True,
-				null=True)
+    venue = fields.ForeignKey(FbLocationResource, 'venue', full=True,
+                              null=True)
 
-		class Meta:
-				queryset = FbEvent.objects.all()
-				resource_name = 'fbevents'
-				allowed_methods = ['get']
-				# fields = [
-				# 	'attending_count',
-				# 	'declined_count',
-				# 	'pic_square'
-				# 	'start_time',
-				# 	'unsure_count']
+    class Meta:
+        queryset = FbEvent.objects.all()
+        resource_name = 'fbevents'
+        allowed_methods = ['get']
+        # fields = [
+        # 	'attending_count',
+        # 	'declined_count',
+        # 	'pic_square'
+        # 	'start_time',
+        # 	'unsure_count']
 
-		def dehydrate(self, bundle):
-				event = bundle.obj
-				bundle.data['name'] = event.common.name
-				if event.pic_cover is not None:
-					bundle.data['pic_cover_source'] = event.pic_cover.source
-				bundle.data['males'] = event.users.filter(sex="male").count()
-				bundle.data['females'] = event.users.filter(sex="female").count()
-				return bundle
+    def dehydrate(self, bundle):
+        event = bundle.obj
+        bundle.data['name'] = event.common.name
+        if event.pic_cover is not None:
+            bundle.data['pic_cover_source'] = event.pic_cover.source
+        bundle.data['males'] = event.users.filter(sex="male").count()
+        bundle.data['females'] = event.users.filter(sex="female").count()
+        return bundle
 
 
 # class FbEventDetailsResource(ModelResource):
